e4b33cc8df36dd819a79cb27370f86fc0567be12
Villex <victor.bpt04@gmail.com>
2025-06-10 11:16:37 -0300

    Adding classes generated from StartUML

---
+++ code/level.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class Level:
+    def __init__(self):
+        self.windwo = None
+        self.name = None
+        self.entity_list = None
+
+    def run(self, ):
+        pass
