992f21d2d71cb8cb52828acc80a3d6d87921c60d
Villex <victor.bpt04@gmail.com>
2025-06-27 14:44:22 -0300

    Creating menu interations Creating ABC class Creating Factory Level 1 bg implementation: Parallax

--- code/entity.py
+++ code/entity.py
@@ -1,11 +1,27 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from abc import ABC, abstractmethod
+import pygame as pg
+import os
 
-class Entity:
-    def __init__(self):
-        self.name = None
-        self.surf = None
-        self.rect = None
+class Entity(ABC):
+    def __init__(self, name: str, position: tuple):
+        self.name = name
+        base_path = os.path.dirname(__file__)
+        image_path = os.path.join(base_path, '..', 'asset', name + '.png')
+        if not os.path.exists(image_path):
+            raise FileNotFoundError(f"Imagem '{name}.png' não encontrada no caminho: {image_path}")
+        self.surf = pg.image.load(image_path)
+        self.rect = self.surf.get_rect(left=position[0], top=position[1])
+        self.speed = 0
 
+    @abstractmethod #decorator
     def move(self, ):
         pass
+
+
+# base_path = os.path.dirname(__file__)
+# image_path = os.path.join(base_path, '..', 'asset', name + '.png')
+# if not os.path.exists(image_path):
+#     raise FileNotFoundError(f"Imagem '{name}.png' não encontrada no caminho: {image_path}")
+# self.surf = pg.image.load(image_path)
--- code/level.py
+++ code/level.py
@@ -1,11 +1,27 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from ctypes import windll
+import pygame as pg
+from project.code.Entity import Entity
+from project.code.EntityFactory import EntityFactory
+
 
 class Level:
-    def __init__(self):
-        self.windwo = None
-        self.name = None
-        self.entity_list = None
+    def __init__(self, window, name, game_mode):
+        self.window = window
+        self.name = name
+        self. game_mode = game_mode
+        self.entity_list: list[Entity] = []
+        self.entity_list.extend(EntityFactory.get_entity('Level1Bg'))
+        print("Entidades carregadas:", len(self.entity_list))
 
-    def run(self, ):
-        pass
+    def run(self):
+        clock = pg.time.Clock()
+        while True:
+            self.window.fill((0, 0, 0))
+            for ent in self.entity_list:
+                self.window.blit(source=ent.surf, dest=ent.rect)
+                ent.move()
+            pg.display.flip()
+            clock.tick(60)  # 60 FPS
+            pg.draw.rect(self.window, (255, 0, 0), (0, 0, 50, 50))  # retângulo vermelho
